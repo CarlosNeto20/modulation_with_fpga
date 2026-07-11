5292f69a2ba22d83bade442cabef7aed33f9830a
CarlosNeto20 <cneto3240@gmail.com>
2026-07-11 19:02:34 -0300

    Melhor resolução da moduladora senoidal e ajustes para o ModelSim Altera

--- senoide.vhd
+++ senoide.vhd
@@ -33,13 +33,18 @@ end entity;
 architecture rtl of senoide is
 
     -- Memória ROM (LUT) de 32 pontos para a senoide
-    type rom_type is array (0 to 31) of integer range 0 to 1023;
+    type rom_type is array (0 to 127) of integer range 0 to 1023;
     
+    -- Valores calculados para senoide de 0 a 1023 (512 +/- 511)
     constant SINE_LUT : rom_type := (
-        512, 612, 708, 796, 873, 937, 984, 1013, 
-        1023, 1013, 984, 937, 873, 796, 708, 612, 
-        512, 412, 316, 228, 151,  87,  40,  11, 
-          1,  11,  40,  87, 151, 228, 316, 412
+        512, 537, 562, 587, 612, 637, 661, 685, 708, 731, 753, 775, 796, 816, 836, 855,
+        873, 890, 906, 922, 937, 950, 963, 974, 984, 993, 1001, 1007, 1013, 1017, 1020, 1022,
+        1023, 1022, 1020, 1017, 1013, 1007, 1001, 993, 984, 974, 963, 950, 937, 922, 906, 890,
+        873, 855, 836, 816, 796, 775, 753, 731, 708, 685, 661, 637, 612, 587, 562, 537,
+        512, 487, 462, 437, 412, 387, 363, 339, 316, 293, 271, 249, 228, 208, 188, 169,
+        151, 134, 118, 102, 87, 74, 61, 50, 40, 31, 23, 17, 11, 7, 4, 2,
+        1, 2, 4, 7, 11, 17, 23, 31, 40, 50, 61, 74, 87, 102, 118, 134,
+        151, 169, 188, 208, 228, 249, 271, 293, 316, 339, 363, 387, 412, 437, 462, 487
     );
 
     -- Acumulador de Fase (DDS) de 32 bits
@@ -53,7 +58,7 @@ architecture rtl of senoide is
 	 -- Se for ver no osciloscópio, comente a linha a baixo e use a linha de cima.
 	 constant PASSO : unsigned(31 downto 0) := to_unsigned(858993, 32);
 
-    signal lut_index : integer range 0 to 31;
+    signal lut_index : integer range 0 to 127;
 
 begin
 
@@ -67,7 +72,7 @@ begin
     end process;
 
     -- Extrai os 5 bits mais significativos para varrer a LUT de 32 posições
-    lut_index <= to_integer(phase_acc(31 downto 27));
+    lut_index <= to_integer(phase_acc(31 downto 25));
 
     sen <= SINE_LUT(lut_index);
 
